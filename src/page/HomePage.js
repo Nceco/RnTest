13eab35e424123d04097e64f86dbc65c3f506c5d
tangc <169713467@qq.com>
2021-06-24 23:05:19 +0800

    tabbar初搭

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react'
-import {View, TouchableOpacity,Text,StyleSheet} from 'react-native'
+import {View, TouchableOpacity,Text,StyleSheet,Image} from 'react-native'
 import {Na} from 'react-navigation'
 import { DefaultBackgroundColor, getThemColor } from '../../utils/styles'
 import {getScreenWidth} from '../../utils/util'
+import TabNavigator from 'react-native-tab-navigator'
+import { iosBottom } from '../../utils/ios'
 
 const styles = StyleSheet.create({
     container:{
@@ -32,11 +34,44 @@ const styles = StyleSheet.create({
 class HomePage extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            title:'首页'
+        }
     }
     render(){
         return (
             <View style={styles.container}>
-                <View style={styles.btnViewStyle}>
+                <TabNavigator
+                    tabBarStyle={{alignItems:'center',height:53+iosBottom,paddingBottom:iosBottom}}
+                >
+                    <TabNavigator.Item
+                        selected={this.state.title === '首页'}
+                        title={'首页'}
+                        titleStyle={{fontSize:10,color:'#333333'}}
+                        selectedTitleStyle={{fontSize:10,color:getThemColor()}}
+                        renderIcon={() => <Image source={require('../resource/images/home.png')} style={{width:26,height:26}}/>}
+                        renderSelectedIcon={() => <Image source={require('../resource/images/home_selected.png')} style={{width:26,height:26}}/>}
+                        onPress={() => {this.setState({title:'首页'})}}
+                    >
+                        <View>
+                            <Text>首页</Text>
+                        </View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.title === '我的'}
+                        title={'我的'}
+                        titleStyle={{fontSize:10,color:'#333333'}}
+                        selectedTitleStyle={{fontSize:10,color:getThemColor()}}
+                        renderIcon={() => <Image source={require('../resource/images/my.png')} style={{width:26,height:26}}/>}
+                        renderSelectedIcon={() => <Image source={require('../resource/images/my_selected.png')} style={{width:26,height:26}}/>}
+                        onPress={() => {this.setState({title:'我的'})}}
+                    >
+                        <View>
+                            <Text>我的</Text>
+                        </View>
+                    </TabNavigator.Item>
+                </TabNavigator>
+                {/* <View style={styles.btnViewStyle}>
                     <TouchableOpacity 
                         style={styles.btnStyle}
                         onPress={() => {
@@ -52,8 +87,8 @@ class HomePage extends Component{
                         }}
                     >
                         <Text style={styles.btnTextStyle}>GoPageTwo</Text>
-                    </TouchableOpacity>
-                </View>
+                    </TouchableOpacity> */}
+                {/* </View> */}
             </View>
         )
     }
